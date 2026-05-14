1062b62f4570685a78c8165056fbfae6e68adc02
User <User@DESKTOP-HS4G4KD>
2026-05-14 21:55:37 +0300

    Proje dosyası ekle.

---
+++ OtelRezervasyon/DatabaseHelper.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+
+public class DatabaseHelper
+{
+    private static string connectionString =
+        "Server=localhost\\SQLEXPRESS;Database=OtelRezervasyon;Trusted_Connection=True;";
+
+    public static SqlConnection GetConnection()
+    {
+        return new SqlConnection(connectionString);
+    }
+}
---
+++ OtelRezervasyon/Form1.cs
@@ -0,0 +1,10 @@
+namespace OtelRezervasyon
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
